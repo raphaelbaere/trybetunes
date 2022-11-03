07046bfd1558885abfdb56428e6e89d876993e08
raphaelbaere <raphaelbaere@id.uff.br>
2022-11-03 15:57:51 -0300

    Requisito 2 concluído

--- src/App.js
+++ src/App.js
@@ -2,18 +2,69 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
+import Loading from './pages/Loading';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 import ProfileEdit from './pages/ProfileEdit';
 import Search from './pages/Search';
+import { createUser } from './services/userAPI';
 
 class App extends React.Component {
+  state = {
+    loginNameInput: '',
+    isLoginButtonDisabled: true,
+    loading: false,
+    redirect: false,
+  };
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  onInputChange = ({ target }) => {
+    const minInputLength = 3;
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+    if (name === 'loginNameInput' && value.length >= minInputLength) {
+      this.setState({
+        isLoginButtonDisabled: false,
+      });
+    } else {
+      this.setState({
+        isLoginButtonDisabled: true,
+      });
+    }
+  };
+
+  onEnterLoginClick = async () => {
+    const { loginNameInput } = this.state;
+    this.setState({ loading: true });
+    await createUser({
+      name: loginNameInput,
+    });
+    this.setState({ loading: false, redirect: true });
+  };
+
   render() {
+    const { loginNameInput, isLoginButtonDisabled, loading, redirect } = this.state;
     return (
       <div className="app">
         <Switch>
-          <Route exact path="/"><Login /></Route>
+          <Route exact path="/">
+            <Login
+              loginNameInput={ loginNameInput }
+              isLoginButtonDisabled={ isLoginButtonDisabled }
+              onEnterLoginClick={ this.onEnterLoginClick }
+              onInputChange={ this.onInputChange }
+              loading={ loading }
+              onFormSubmit={ this.onFormSubmit }
+              redirect={ redirect }
+            />
+          </Route>
+          <Route path="/loading"><Loading /></Route>
           <Route path="/search"><Search /></Route>
           <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
           <Route path="/favorites"><Favorites /></Route>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,54 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
 
 export default class Login extends Component {
   render() {
+    const {
+      onEnterLoginClick,
+      loginNameInput,
+      isLoginButtonDisabled,
+      onInputChange,
+      loading,
+      onFormSubmit,
+      redirect } = this.props;
     return (
-      <div data-testid="page-login">Login</div>
+      <div data-testid="page-login">
+        { loading ? <Loading /> : (
+          <form onSubmit={ onFormSubmit }>
+            <label htmlFor="login-name-input">
+              Nome
+              <input
+                id="login-name-input"
+                data-testid="login-name-input"
+                type="text"
+                name="loginNameInput"
+                value={ loginNameInput }
+                onChange={ onInputChange }
+              />
+            </label>
+            <button
+              disabled={ isLoginButtonDisabled }
+              onClick={ onEnterLoginClick }
+              type="button"
+              data-testid="login-submit-button"
+            >
+              Entrar
+            </button>
+          </form>)}
+        { redirect && <Redirect to="/search" />}
+      </div>
     );
   }
 }
+
+Login.propTypes = {
+  isLoginButtonDisabled: PropTypes.bool.isRequired,
+  loading: PropTypes.bool.isRequired,
+  loginNameInput: PropTypes.string.isRequired,
+  onEnterLoginClick: PropTypes.func.isRequired,
+  onFormSubmit: PropTypes.func.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+  redirect: PropTypes.bool.isRequired,
+};
