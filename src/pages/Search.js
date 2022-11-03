3937608b17cda9a16cd84d22cd66ea2fa3a5d8aa
raphaelbaere <raphaelbaere@id.uff.br>
2022-11-03 18:16:07 -0300

    Requisito 5 concluído

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,49 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 export default class Search extends Component {
+  state = {
+    searchArtistInput: '',
+    isSearchArtistButtonDisabled: true,
+  };
+
+  onInputChange = ({ target }) => {
+    const { name, value } = target;
+    const minLength = 2;
+    this.setState({
+      [name]: value,
+    });
+    if (name === 'searchArtistInput' && value.length >= minLength) {
+      this.setState({
+        isSearchArtistButtonDisabled: false,
+      });
+    }
+  };
+
   render() {
+    const { searchArtistInput, isSearchArtistButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="searchArtistInput">
+            Search for artists
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              name="searchArtistInput"
+              value={ searchArtistInput }
+              onChange={ this.onInputChange }
+              id="searchArtistInput"
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ isSearchArtistButtonDisabled }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
